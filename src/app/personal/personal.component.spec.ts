c92ee9c3d1ca369bbee514423d281e995f924ded
Puchigay <afx3v6@gmail.com>
2023-05-12 21:37:57 -0500

    funciona xD

--- src/app/personal/personal.component.spec.ts
+++ src/app/personal/personal.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule  } from '@angular/forms';
 
 import { PersonalComponent } from './personal.component';
 
@@ -8,6 +9,7 @@ describe('PersonalComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports:[FormsModule],
       declarations: [ PersonalComponent ]
     })
     .compileComponents();
